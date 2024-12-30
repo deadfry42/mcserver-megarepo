22193c3cdf7b1af47d976d555b5103ff167aad7f
deadfry42 <pooface442@gmail.com>
2024-12-30 21:33:18 +0000

    DFChatImprovments - Add emojis 0xE913-E922

--- DFChatImprovements/src/main/java/uk/co/nikodem/dFChatImprovements/DFChatImprovements.java
+++ DFChatImprovements/src/main/java/uk/co/nikodem/dFChatImprovements/DFChatImprovements.java
@@ -42,7 +42,7 @@ public final class DFChatImprovements extends JavaPlugin implements Listener {
                     case "skull" -> msg = msg.replace(word, "\uE900");
                     case "cold_sunglasses" -> msg = msg.replace(word, "\uE901");
                     case "sob" -> msg = msg.replace(word, "\uE902");
-                    case "flag_pl" -> msg = msg.replace(word, "\uE903");
+                    case "chair" -> msg = msg.replace(word, "\uE903");
                     case "sunglasses" -> msg = msg.replace(word, "\uE904");
                     case "smile" -> msg = msg.replace(word, "\uE905");
                     case "money_mouth", "money_mouth_face" -> msg = msg.replace(word, "\uE906");
@@ -58,11 +58,29 @@ public final class DFChatImprovements extends JavaPlugin implements Listener {
                     case "salute" -> msg = msg.replace(word, "\uE910");
                     case "shrug" -> msg = msg.replace(word, "\uE911");
                     case "wompwomp" -> msg = msg.replace(word, "\uE912");
+                    case "joobi", "cold_cry" -> msg = msg.replace(word, "\uE913");
+                    case "wave", "waving_hand" -> msg = msg.replace(word, "\uE914");
+                    case "pensive", "pensive_face" -> msg = msg.replace(word, "\uE915");
+                    case "scream" -> msg = msg.replace(word, "\uE916");
+                    case "smiling_imp" -> msg = msg.replace(word, "\uE917");
+                    case "neutral_face", "neutral" -> msg = msg.replace(word, "\uE918");
+                    case "slight_smile", "slightly_smiling_face" -> msg = msg.replace(word, "\uE919");
+                    case "flushed" -> msg = msg.replace(word, "\uE91A");
+                    case "astonished" -> msg = msg.replace(word, "\uE91B");
+                    case "face_vomiting", "vomiting" -> msg = msg.replace(word, "\uE91C");
+                    case "scream_cat" -> msg = msg.replace(word, "\uE91D");
+                    case "pleading", "pleading_face" -> msg = msg.replace(word, "\uE91E");
+                    case "angry", "angry_face" -> msg = msg.replace(word, "\uE91F");
+                    case "rage", "rage_face", "pouting_face" -> msg = msg.replace(word, "\uE920");
+                    case "nerd", "nerd_face" -> msg = msg.replace(word, "\uE921");
+                    case "beginner", "japanese_symbol_for_beginner" -> msg = msg.replace(word, "\uE922");
 
                     case null, default -> {
 
                     }
                 }
+
+
             }
         }
 
